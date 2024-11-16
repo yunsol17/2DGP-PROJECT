0900f93ebeff16d8034687ec18fd67bf1d317b7c
yunsol17 <yunsol1007@naver.com>
2024-11-17 00:57:26 +0900

    hp,level 글자 숫자 넣기

--- 2DGP-PROJECT.py
+++ 2DGP-PROJECT.py
@@ -1,10 +1,14 @@
 from pico2d import *
 import random
+import time
 
 BK_WIDTH, BK_HEIGHT = 1080, 879
 dir_x = 0  # x축 이동 방향 (왼쪽: -1, 오른쪽: 1)
 dir_y = 0  # y축 이동 방향 (아래: -1, 위: 1)
 open_canvas(BK_WIDTH, BK_HEIGHT)
+
+fish1_list = []
+last_spawn_time = time.time()
 
 # 배경 클래스
 class BackGround:
@@ -14,6 +18,43 @@ class BackGround:
         pass
     def draw(self):
         self.image.draw(BK_WIDTH // 2, BK_HEIGHT // 2)
+class Level:
+    def __init__(self):
+        self.x, self.y = 130, 150
+        self.frame = 0
+        self.image = load_image('level.png')
+        self.frame_width = self.image.w
+        self.frame_height = self.image.h
+    def update(self):
+        pass
+    def draw(self):
+        self.image.clip_draw(self.frame * self.frame_width, 0,self.frame_width, self.frame_height,self.x, self.y, 150, 70)
+class Hp:
+    def __init__(self):
+        self.x, self.y = 60, 845
+        self.frame = 0
+        self.image = load_image('hp.png')
+        self.frame_width = self.image.w
+        self.frame_height = self.image.h
+    def draw(self):
+        self.image.clip_draw(self.frame * self.frame_width, 0,self.frame_width, self.frame_height,self.x, self.y, 70, 50)
+
+class Num:
+    def __init__(self):
+        self.x, self.y = 130, 83
+        self.frame = 0
+        self.image = load_image('num.png')
+        self.frame_width = self.image.w // 9
+        self.frame_height = self.image.h
+
+    def update(self):
+        pass
+        #self.frame = (self.frame + 1) % 9
+
+    def draw(self):
+            self.image.clip_draw(self.frame * self.frame_width, 0,
+                                 self.frame_width, self.frame_height,
+                                 self.x, self.y, 50, 50)
 # 충돌 체크 함수
 def check_collision(a, b):
     left_a, bottom_a, right_a, top_a = a.get_bb()
@@ -32,34 +73,57 @@ class Whale:
         self.image = load_image('whale.png')
         self.frame_width = self.image.w // 2  # 가로 프레임 2개
         self.frame_height = self.image.h // 4  # 세로 프레임 4개
+        self.is_colliding = False  # 충돌 상태를 나타내는 변수
+        self.collision_time = 0  # 충돌 발생 시간을 기록
+
     def update(self):
         global dir_x, dir_y
-        self.frame = (self.frame + 1) % 2
+        current_time = time.time()
+
+        # 충돌 상태에서 1초 경과 시 초기화
+        if self.is_colliding and current_time - self.collision_time >= 0.1:
+            self.is_colliding = False  # 충돌 상태 해제
+            self.frame = 0  # 첫 번째 줄 애니메이션 초기화
+
+        if not self.is_colliding:  # 충돌 상태가 아닐 때만 기본 프레임 애니메이션
+            self.frame = (self.frame + 1) % 2
         self.x += dir_x * 20  # x축 방향 이동
         self.y += dir_y * 20  # y축 방향 이동
+
         # 화면 밖으로 나가지 않도록 위치 제한
         if self.x < 50:
             self.x = 50
-        elif self.x > BK_WIDTH-50:
-            self.x = BK_WIDTH-50
+        elif self.x > BK_WIDTH - 50:
+            self.x = BK_WIDTH - 50
         if self.y < 230:
             self.y = 230
-        elif self.y > BK_HEIGHT-100:
-            self.y = BK_HEIGHT-100
+        elif self.y > BK_HEIGHT - 100:
+            self.y = BK_HEIGHT - 100
         if dir_x != 0:
             self.direction = dir_x
+
     def draw(self):
-        if self.direction ==-1:
-            draw_rectangle(*self.get_bb())
-            self.image.clip_draw(self.frame * self.frame_width, 3 * self.frame_height, self.frame_width, self.frame_height, self.x, self.y,100,100)
-        else:
-            draw_rectangle(*self.get_bb())
-            self.image.clip_composite_draw(self.frame * self.frame_width, 3 * self.frame_height, self.frame_width, self.frame_height,0,'h', self.x, self.y,100,100)
+        if self.is_colliding:  # 충돌 상태일 때는 두 번째 줄의 첫 번째 프레임 사용
+            if self.direction == -1:
+                self.image.clip_draw(0, 2 * self.frame_height, self.frame_width, self.frame_height,
+                                     self.x, self.y, 100, 100)
+            else:
+                self.image.clip_composite_draw(0, 2 * self.frame_height, self.frame_width, self.frame_height,
+                                               0, 'h', self.x, self.y, 100, 100)
+        else:  # 일반 상태
+            if self.direction == -1:
+                self.image.clip_draw(self.frame * self.frame_width, 3 * self.frame_height, self.frame_width, self.frame_height,
+                                     self.x, self.y, 100, 100)
+            else:
+                self.image.clip_composite_draw(self.frame * self.frame_width, 3 * self.frame_height, self.frame_width, self.frame_height,
+                                               0, 'h', self.x, self.y, 100, 100)
+        draw_rectangle(*self.get_bb())
 
     def get_bb(self):
-        return self.x - 40, self.y - 20, self.x + 50, self.y + 20
-
-
+        if self.direction == -1:  # 왼쪽 방향
+            return self.x - 50, self.y - 20, self.x + 40, self.y + 20
+        else:  # 오른쪽 방향
+            return self.x - 40, self.y - 20, self.x + 50, self.y + 20
 class Squid:
     def __init__(self):
         self.x, self.y = -20, random.randint(230, BK_HEIGHT - 100)
@@ -84,10 +148,10 @@ class Squid:
             draw_rectangle(*self.get_bb())
     def get_bb(self):
         return self.x - 5, self.y - 45, self.x + 40, self.y + 60
-
 class Fish1:
     def __init__(self):
         self.x, self.y = -20, random.randint(230, BK_HEIGHT - 100)
+        self.bx,self.by=350,80
         self.frame = 0
         self.direction = -1
         self.image = load_image('fish1.png')
@@ -108,9 +172,13 @@ class Fish1:
                                            self.frame_width, self.frame_height,
                                            0, 'h', self.x, self.y, 20, 20)
             draw_rectangle(*self.get_bb())
+
+    def drawB(self):
+        self.image.clip_draw(self.frame * self.frame_width, 0,
+                             self.frame_width, self.frame_height,
+                             self.bx, self.by, 20, 20)
     def get_bb(self):
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
-
 class Fish2:
     def __init__(self):
         self.x, self.y = -20, random.randint(230, BK_HEIGHT - 100)
@@ -189,6 +257,11 @@ class Crab:
     def get_bb(self):
         return self.x - 15, self.y - 15, self.x + 15, self.y + 15
 
+def spawn_fish1():
+    global fish1_list
+    count = random.randint(1, 3)  # 2개 또는 3개 생성
+    for _ in range(count):
+        fish1_list.append(Fish1())
 # 이벤트 처리 함수
 def handle_events():
     global swimming, dir_x, dir_y
@@ -221,15 +294,20 @@ def handle_events():
 def reset_world():
     global swimming
     global background
+    global level
+    global num
     global whale
     global squid
     global fish1
     global fish2
     global fish3
     global crab
-
+    global hp
     swimming = True
     background = BackGround()
+    num = Num()
+    hp=Hp()
+    level = Level()
     whale = Whale()
     squid = Squid()
     fish1=Fish1()
@@ -238,18 +316,39 @@ def reset_world():
     crab = Crab()
 # 월드 업데이트
 def update_world():
+    global last_spawn_time
+    current_time = time.time()
+
+    if current_time - last_spawn_time >= 5.0:
+        spawn_fish1()
+        last_spawn_time = current_time
+    num.update()
+    level.update()
     whale.update()
     crab.update()
     fish1.update()
     fish2.update()
     fish3.update()
     squid.update()
+    for fish in fish1_list[:]:
+        fish.update()
+        if check_collision(whale, fish):  # Whale과 Fish1 충돌 체크
+            whale.is_colliding = True
+            whale.collision_time = time.time()
+            fish1_list.remove(fish)  # 충돌한 Fish1 제거
+        elif fish.x < -50 or fish.x > BK_WIDTH + 50:  # 화면 밖으로 나간 경우 제거
+            fish1_list.remove(fish)
 
 # 월드 렌더링
 def render_world():
     clear_canvas()
     background.draw()
-    fish1.draw()
+    hp.draw()
+    num.draw()
+    level.draw()
+    fish1.drawB()
+    for fish in fish1_list:
+        fish.draw()
     fish2.draw()
     fish3.draw()
     squid.draw()
