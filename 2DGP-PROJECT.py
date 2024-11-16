87497a97b9a412c4d431f6f543633b0d29e200ca
yunsol17 <yunsol1007@naver.com>
2024-11-16 19:04:01 +0900

    물고기들 띄우기

--- 2DGP-PROJECT.py
+++ 2DGP-PROJECT.py
@@ -47,6 +47,118 @@ class Whale:
         else:
             self.image.clip_composite_draw(self.frame * self.frame_width, 3 * self.frame_height, self.frame_width, self.frame_height,0,'h', self.x, self.y,100,100)
 
+class Squid:
+    def __init__(self):
+        self.x, self.y = -20, random.randint(230, BK_HEIGHT - 100)
+        self.frame = 0
+        self.direction = random.choice([-1, 1])  # 랜덤 방향 선택
+        self.image = load_image('squid.png')
+        self.frame_width = self.image.w // 5  # 스프라이트 가로 분할
+        self.frame_height = self.image.h
+
+    def update(self):
+        self.frame = (self.frame + 1) % 5  # 프레임 수는 스프라이트 가로 프레임 수에 맞추세요
+        self.x += 5
+
+    def draw(self):
+        if self.direction == -1:
+            self.image.clip_draw(self.frame * self.frame_width, 0,
+                                 self.frame_width, self.frame_height,
+                                 self.x, self.y, 50, 50)  # 크기 조정
+        else:
+            self.image.clip_composite_draw(self.frame * self.frame_width, 0,
+                                           self.frame_width, self.frame_height,
+                                           0, 'h', self.x, self.y, 50, 50)
+class Fish1:
+    def __init__(self):
+        self.x, self.y = -20, random.randint(230, BK_HEIGHT - 100)
+        self.frame = 0
+        self.direction = -1
+        self.image = load_image('fish1.png')
+        self.frame_width = self.image.w
+        self.frame_height = self.image.h
+
+    def update(self):
+        self.x += 5
+
+    def draw(self):
+        if self.direction == -1:
+            self.image.clip_draw(self.frame * self.frame_width, 0,
+                                 self.frame_width, self.frame_height,
+                                 self.x, self.y, 20, 20)  # 크기 조정
+        else:
+            self.image.clip_composite_draw(self.frame * self.frame_width, 0,
+                                           self.frame_width, self.frame_height,
+                                           0, 'h', self.x, self.y, 20, 20)
+
+class Fish2:
+    def __init__(self):
+        self.x, self.y = -20, random.randint(230, BK_HEIGHT - 100)
+        self.frame = 0
+        self.direction = -1
+        self.image = load_image('fish2.png')
+        self.frame_width = self.image.w
+        self.frame_height = self.image.h
+
+    def update(self):
+        self.x += 5
+
+    def draw(self):
+        if self.direction == -1:
+            self.image.clip_draw(self.frame * self.frame_width, 0,
+                                 self.frame_width, self.frame_height,
+                                 self.x, self.y, 40, 40)  # 크기 조정
+        else:
+            self.image.clip_composite_draw(self.frame * self.frame_width, 0,
+                                           self.frame_width, self.frame_height,
+                                           0, 'h', self.x, self.y, 40, 40)
+
+class Fish3:
+    def __init__(self):
+        self.x, self.y = -20, random.randint(230, BK_HEIGHT - 100)
+        self.frame = 0
+        self.direction = -1
+        self.image = load_image('fish3.png')
+        self.frame_width = self.image.w // 5  # 스프라이트 가로 분할
+        self.frame_height = self.image.h
+
+    def update(self):
+        self.frame = (self.frame + 1) % 5  # 프레임 수는 스프라이트 가로 프레임 수에 맞추세요
+        self.x += 5
+
+    def draw(self):
+        if self.direction == -1:
+            self.image.clip_draw(self.frame * self.frame_width, 0,
+                                 self.frame_width, self.frame_height,
+                                 self.x, self.y, 80, 80)  # 크기 조정
+        else:
+            self.image.clip_composite_draw(self.frame * self.frame_width, 0,
+                                           self.frame_width, self.frame_height,
+                                           0, 'h', self.x, self.y, 80, 80)
+
+class Crab:
+    def __init__(self):
+        self.x, self.y = -20, random.randint(230, BK_HEIGHT - 100)
+        self.frame = 0
+        self.direction = -1
+        self.image = load_image('crab.png')
+        self.frame_width = self.image.w // 3  # 스프라이트 가로 분할
+        self.frame_height = self.image.h
+
+    def update(self):
+        self.frame = (self.frame + 1) % 3  # 프레임 수는 스프라이트 가로 프레임 수에 맞추세요
+        self.x += 5
+
+    def draw(self):
+        if self.direction == -1:
+            self.image.clip_draw(self.frame * self.frame_width, 0,
+                                 self.frame_width, self.frame_height,
+                                 self.x, self.y, 30, 30)  # 크기 조정
+        else:
+            self.image.clip_composite_draw(self.frame * self.frame_width, 0,
+                                           self.frame_width, self.frame_height,
+                                           0, 'h', self.x, self.y, 30, 30)
+
 # 이벤트 처리 함수
 def handle_events():
     global swimming, dir_x, dir_y
@@ -80,18 +192,38 @@ def reset_world():
     global swimming
     global background
     global whale
+    global squid
+    global fish1
+    global fish2
+    global fish3
+    global crab
+
     swimming = True
     background = BackGround()
     whale = Whale()
-
+    squid = Squid()
+    fish1=Fish1()
+    fish2=Fish2()
+    fish3=Fish3()
+    crab = Crab()
 # 월드 업데이트
 def update_world():
     whale.update()
+    crab.update()
+    fish1.update()
+    fish2.update()
+    fish3.update()
+    squid.update()
 
 # 월드 렌더링
 def render_world():
     clear_canvas()
     background.draw()
+    fish1.draw()
+    fish2.draw()
+    fish3.draw()
+    squid.draw()
+    crab.draw()
     whale.draw()  # 고래 그리기 추가
     update_canvas()
 
@@ -105,7 +237,7 @@ while swimming:
     handle_events()
     update_world()
     render_world()
-    delay(0.05)
+    delay(0.07)
 
 # 종료 코드
 close_canvas()
